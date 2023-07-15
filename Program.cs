daf753e823fee0c0d1261e24e9a26bc239146a83
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-07-15 15:53:06 +0300

    Hangfire and swagger is installed.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
 using Hangfire.MemoryStorage;
 using Hangfire;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movie API", Version = "v1" });
+});
 builder.Services.AddControllers();
 builder.Services.AddHangfire(options => options.UseMemoryStorage());
 builder.Services.AddHangfireServer();
 JobStorage.Current = new MemoryStorage();
 
+
+
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
+Console.WriteLine($"Urls from Program.cs before app.StartAsync(): {string.Join(", ", app.Urls)}");
+app.UseSwagger();
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Movie API V1");
+});
 app.UseHangfireDashboard();
 app.UseAuthorization();
 
